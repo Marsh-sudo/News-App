a7479aba0da153ca84da0aec6f2ae70383d2f388
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-26 18:58:11 +0300

    created files for sources

---
+++ app/models/news.py
@@ -0,0 +1,12 @@
+class Sources:
+    '''
+    source class to define source objects
+    '''
+
+    def __init__(self,id,name,description,url,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.language = language
+        self.country = country

d7a0b71494542f757c555784e23b83009e3a5e0c
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-26 17:35:02 +0300

    created urls

--- app/config.py
+++ app/config.py
@@ -5,7 +5,10 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q=Apple&from=2022-02-26&sortBy=popularity&apiKey=https://newsapi.org/v2/everything?q=apple&from=2022-02-25&to=2022-02-25&sortBy=popularity&apiKey=9762e5f31e314775bd934aa2b1ef72c1'
+    SOURCES_API_BASE_URL ='GET https://newsapi.org/v2/top-headlines/sources?country=usapiKey=9762e5f31e314775bd934aa2b1ef72c1'
     pass
+
 
 class ProdConfig(Config):
     '''

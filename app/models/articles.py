c1f4d5b40997925b5386cbfd3977593379698cd8
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-28 11:03:03 +0300

    created a fourowfour function

---
+++ app/models/articles.py
@@ -0,0 +1,14 @@
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+
+    def __init__(self,id,author,title,description,url):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.url = url
+        
+
+    

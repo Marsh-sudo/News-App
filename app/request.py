5ad5606e3a3bc008441bcee54b0dade39d8d96ed
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-28 11:00:50 +0300

    created a function

--- app/request.py
+++ app/request.py
@@ -1,16 +1,20 @@
+from email.mime import base
 import json
+from turtle import title
 from unicodedata import name
 
 import urllib3
+
 from app import app
-from urllib.request import json
-from .models import news
+from urllib3.request import json
+from .models import news,articles
 
 #getting api key
 api_key = app.config['NEWS_API_KEY']
 
 #getting the base url
 base_url = app.config['SOURCE_API_BASE_URL']
+base_url = app.config['ARTICLES_API_BASE_URL']
 
 def get_sources(category):
     '''
@@ -55,3 +59,38 @@ def process_results(sources_list):
 
 
     return sources_results
+
+
+def get_articles(category):
+    '''
+    function that gets the json response to our Url request
+    '''
+    get_articles_url = base_url.format(category,api_key)
+
+    with urllib3.request.urlopen(get_articles_url)as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['results']:
+            articles_results_list = get_articles_response['results']
+            articles_results = process_results(articles_results_list)
+
+            return articles_results
+
+
+
+def process_results(articles_list):
+    '''
+    function that process the articles results and transform them to a list of objects 
+    '''
+
+    articles_results = []
+    for article_item in articles_list:
+        id = article_item('id')
+        title = article_item('title')
+        author = article_item('author')
+        description = article_item('description')
+        url = article_item('url')
+

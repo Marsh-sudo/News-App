6bfd0938313a4aec60f7309d9672c0d9aafae105
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-02 17:46:38 +0300

    remove commented syntax

--- app/__init__.py
+++ app/__init__.py
@@ -27,19 +27,3 @@ def create_app(config_name):
     return app
 
 
-# bootstrap =Bootstrap()
-# #initializing application
-
-# app = Flask(__name__,instance_relative_config = True) 
-# from app.main import routes
-
-
-# #setting up configurations
-# app.config.from_object([DevConfig])
-# app.config.from_pyfile("config.py")
-
-# #initializing flask extension
-# bootstrap.init_app(app)
-
-# from app.main import error
-
